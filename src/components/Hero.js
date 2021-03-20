df88b74029f523992d3a6d691be3dddc0c839163
dave <devmuangi@gmail.com>
2021-03-20 15:52:56 +0300

    configuration

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+
+import styles from "../styles/HomeItem.module.css";
+
+
+const Hero = ({}) => {
+
+    return(
+        <>
+        <h1>herroo</h1>
+        </>
+ 
+        
+      
+    )
+}
+export default Hero;
