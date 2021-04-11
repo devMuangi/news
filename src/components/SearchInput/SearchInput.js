604a905d48faa3c3d70828bb02b0a28b3b5635f4
dave <devmuangi@gmail.com>
2021-04-11 11:48:16 +0300

    config

---
+++ src/components/SearchInput/SearchInput.js
@@ -0,0 +1,11 @@
+import styles from "./searchInput.module.css";
+
+export default class searchInput extends Component {
+    render() {
+        return (
+            <div>
+                <input />
+            </div>
+        )
+    }
+}
